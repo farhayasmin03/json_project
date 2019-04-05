08afec9d08b70090ca7778b00477a9237e9ce82e
Farhana Yasmin <farhayasmin03@gmail.com>
2019-04-05 19:36:53 +0530

    added changes

--- app.js
+++ app.js
@@ -95,26 +95,43 @@ var myobj2 = {
 
 
 }
-
+var sameData = []
+var differentData = []
 keyObj1 = Object.keys(myobj1);
 keyObj2 = Object.keys(myobj2);
 // find values
 valueObj1 = Object.values(myobj1);
 valueObj2 = Object.values(myobj2);
 
-if(keyObj1.length > keyObj2.length) {
+if (keyObj1.length > keyObj2.length) {
   var biggestKey = keyObj1.length;
 } else {
   var biggestKey = keyObj2.length;
 }
-for(var i=0; i<biggestKey; i++) {
+/* for(var i=0; i<biggestKey; i++) {
   if(keyObj1[i] == keyObj2[i] && valueObj1[i] == valueObj2[i]) {
     console.log(valueObj2[i]);	
   } else {
     // it prints keys have different values
     console.log('myObj1 value: '+ valueObj1[i] + '\nmyObj2 value: '+ valueObj2[i] +'\n');
+    
   }
-}	
+}	 
+ */
+for (var i = 0; i < biggestKey; i++) {
+  if (keyObj1[i] == keyObj2[i] && valueObj1[i] == valueObj2[i]) {
+    sameData.push(keyObj1[i], valueObj1[i])
+    console.log("sameData")
+    console.log(sameData)
+    
+  } else {
+    console.log("differentData")
+    console.log(differentData)
+    
+  }
+
+  
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
